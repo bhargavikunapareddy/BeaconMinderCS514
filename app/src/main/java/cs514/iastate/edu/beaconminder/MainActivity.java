b7d17057cb4f1d6cbe6defbf710b2da91a20acb6
Nishanth Sivakumar <nishanth@iastate.edu>
2016-11-28 05:09:04 -0600

    Added gimbal dependencies. Added logic for Beacon sighting and toggle item flag.

--- app/src/main/java/cs514/iastate/edu/beaconminder/MainActivity.java
+++ app/src/main/java/cs514/iastate/edu/beaconminder/MainActivity.java
@@ -4,9 +4,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
     public final static String EXTRA_BEACON_ID = "edu.iastate.com514.BEACON_ID";
@@ -14,17 +17,19 @@ public class MainActivity extends AppCompatActivity {
     private List<ItemData> sourceItem;
     private RecyclerView mRecyclerView;
     private MyRecyclerViewAdapter adapter;
-
+    private TextView logText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         sourceItem = new ArrayList<>();
-        ItemData item1 = new ItemData("wallet", "aaaaaaaaaa", "Wallet", R.drawable.wallet, R.drawable.online);
+        ItemData item1 = new ItemData("wallet", "aaaaaaaaaa", "Wallet", R.drawable.wallet, R.drawable.offline);
         ItemData item2 = new ItemData("keys", "bbbbbbbbbb", "Keys", R.drawable.keys, R.drawable.online);
         ItemData item3 = new ItemData("glasses", "cccccccccc", "Glasses", R.drawable.glasses, R.drawable.offline);
-        ItemData item4 = new ItemData("meds", "dddddddddd", "Meds",R.drawable.meds, R.drawable.offline);
+        final ItemData item4 = new ItemData("meds", "dddddddddd", "Meds",R.drawable.meds, R.drawable.offline);
+        final Map<String,ItemData> beaconMap = new HashMap< >();
+        beaconMap.put("X3VP-G6UR1", item1);
 //        ItemData item5 = new ItemData("meds", R.drawable.meds, R.drawable.offline);
 //        ItemData item6 = new ItemData("meds", R.drawable.meds, R.drawable.offline);
 //        ItemData item7 = new ItemData("meds", R.drawable.meds, R.drawable.offline);
@@ -48,6 +53,15 @@ public class MainActivity extends AppCompatActivity {
         mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
         adapter = new MyRecyclerViewAdapter(MainActivity.this, sourceItem);
         mRecyclerView.setAdapter(adapter);
+        logText = (TextView)findViewById(R.id.textView);
+        logText.setText("Place Holder");
+        InitializeBeacon.intializeGimbal(this.getApplication());
+        InitializeBeacon.initializeBeaconMapping(beaconMap, mRecyclerView, adapter, logText);
+    }
 
+    @Override
+    protected void onDestroy() {
+        InitializeBeacon.destroyGimbal(this.getApplication());
+        super.onDestroy();
     }
 }
